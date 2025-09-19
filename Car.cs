378cb3d155bf384035ce024eac2d3a3ab5386ee0
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-19 20:48:38 +0300

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_8_Alecseev_ISP_232_ISRPO
+{
+    internal class Car
+    {
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private int Year { get; set; }
+        private int Mileage { get; set; }
+
+        public Car(string brand, string model, int year, int mileage)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Бренд: {Brand}");
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Год выпуска: {Year}");
+            Console.WriteLine($"Пробег: {Mileage}");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Laba_8_Alecseev_ISP_232_ISRPO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //Car myCar = new("Лада", "granta", 2025, 20000);
+            //myCar.PrintInfo();
+            //Car myCar2 = new("Лада", "vesta", 2024, 40000);
+            //myCar2.PrintInfo();
+        }
+    }
+}
