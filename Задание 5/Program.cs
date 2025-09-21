4a47b73a68c6b522ed8e732bda9672da0f8cb793
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-21 09:29:57 +0300

    Задание 5

---
+++ Задание 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_8_Alecseev_ISP_232_ISRPO.Задание_5
+{
+    //class System
+    //{
+    //    public void Print()
+    //    {
+    //        Console.WriteLine("Hello");
+    //    }
+    //}
+    internal class Program
+    {
+        //static void Main(string[] args) {
+        //    //System.Console.WriteLine("Hello");
+        //    //Ошибка, потому что есть глобальная System и в нашем проекте класс System.
+        //    global::System.Console.WriteLine("Hello World!");
+
+        //    System s = new System();
+        //    s.Print();
+        //}
+    }
+}
