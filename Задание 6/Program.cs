6c760db7d73c389e73641d429b9870fdcb3542bb
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-21 09:48:59 +0300

    Задание 6

---
+++ Задание 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_8_Alecseev_ISP_232_ISRPO.Задание_6
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Rectangle r1 = new Rectangle(50, 10);
+        //    r1.PrintInfo();
+
+        //    Rectangle r2 = new Rectangle(-7, 0);
+        //    r2.PrintInfo();
+        //}
+
+    }
+}
---
+++ Задание 6/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_8_Alecseev_ISP_232_ISRPO.Задание_6
+{
+    internal class Rectangle
+    {
+        private double width; 
+        private double height;
+
+        public double Width 
+        {
+            get { return width; }
+            set 
+            {
+                if (value <= 0) { this.width = 1; }
+                else { this.width = value; }
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0) { this.height = 1; }
+                else { this.height= value; }
+            }
+        }
+
+        public double Area { get { return width * height; } }
+        public Rectangle()
+        {
+            this.width = 1;
+            this.height = 1;
+        }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Ширина: {width}. Высота: {height}. Площадь: {Area}.");
+        }
+
+    }
+}
